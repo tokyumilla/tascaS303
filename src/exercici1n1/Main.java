f2d4374dcecc0da2f0a1aa14bfd1fa8aee61f58b
tokyumilla <tokyumilla@gmail.com>
2023-03-16 18:34:56 +0100

    creación de clase Tool

--- src/exercici1n1/Main.java
+++ src/exercici1n1/Main.java
@@ -3,6 +3,7 @@ package exercici1n1;
 import exercici1n1.connection.Connection;
 import exercici1n1.entities.*;
 import exercici1n1.functionalities.Ticket;
+import exercici1n1.functionalities.Tool;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,11 +12,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
+        Tool tool = Tool.getInstance();
         int option;
         Scanner sc = new Scanner(System.in);
         ArrayList<FlowerShop> flowerShops;
-        flowerShops = readFile();
+        flowerShops = tool.readFile();
 
 
         do {
@@ -35,38 +36,38 @@ public class Main {
 
             switch (option) {
                 case 1:
-                    createFlowerShop(flowerShops);
-                    writeFile(flowerShops);
+                    tool.createFlowerShop(flowerShops);
+                    tool.writeFile(flowerShops);
                     break;
                 case 2:
-                    showFlowerShops(flowerShops);
+                    tool.showFlowerShops(flowerShops);
                     break;
                 case 3:
                     System.out.println("¿A qué floristería quiere añadir el producto?");
-                    FlowerShop flowerShop1 = findFlowerShop(flowerShops);
+                    FlowerShop flowerShop1 = tool.findFlowerShop(flowerShops);
                     if (flowerShop1 != null) {
-                        addProduct(flowerShop1);
+                        tool.addProduct(flowerShop1);
                     }
-                    writeFile(flowerShops);
+                    tool.writeFile(flowerShops);
                     break;
                 case 4:
                     System.out.println("¿De qué floristería quiere consultar el stock?");
-                    FlowerShop flowerShop2 = findFlowerShop(flowerShops);
+                    FlowerShop flowerShop2 = tool.findFlowerShop(flowerShops);
                     if (flowerShop2 != null) {
-                        showStock(flowerShop2);
+                        tool.showStock(flowerShop2);
                     }
                     break;
                 case 5:
                     System.out.println("¿De qué floristería quiere retirar el producto?");
-                    FlowerShop flowerShop3 = findFlowerShop(flowerShops);
+                    FlowerShop flowerShop3 = tool.findFlowerShop(flowerShops);
                     if (flowerShop3 != null) {
-                        deleteProduct(flowerShop3);
-                        writeFile(flowerShops);
+                        tool.deleteProduct(flowerShop3);
+                        tool.writeFile(flowerShops);
                     }
                     break;
                 case 6:
                     System.out.println("¿De qué floristería quiere consultar el valor total?");
-                    FlowerShop flowerShop4 = findFlowerShop(flowerShops);
+                    FlowerShop flowerShop4 = tool.findFlowerShop(flowerShops);
                     if (flowerShop4 != null) {
                         System.out.println("El valor total del stock de la floristería es " +
                                 flowerShop4.getStockValue());
@@ -74,25 +75,25 @@ public class Main {
                     break;
                 case 7:
                     System.out.println("¿En qué floristería quiere comprar?");
-                    FlowerShop flowerShop5 = findFlowerShop(flowerShops);
+                    FlowerShop flowerShop5 = tool.findFlowerShop(flowerShops);
                     if (flowerShop5 != null) {
                         Ticket ticket = new Ticket();
-                        buyVoid(flowerShop5, ticket);
+                        tool.buyVoid(flowerShop5, ticket);
                         ticket.printTicket();
                         flowerShop5.addTickets(ticket);
-                        writeFile(flowerShops);
+                        tool.writeFile(flowerShops);
                     }
                     break;
                 case 8:
                     System.out.println("¿De qué floristería quiere ver el histórico de compras?");
-                    FlowerShop flowerShop6 = findFlowerShop(flowerShops);
+                    FlowerShop flowerShop6 = tool.findFlowerShop(flowerShops);
                     if (flowerShop6 != null) {
                         flowerShop6.showSales();
                     }
                     break;
                 case 9:
                     System.out.println("¿De qué floristería quieres saber el total de ventas?");
-                    FlowerShop flowerShop7 = findFlowerShop(flowerShops);
+                    FlowerShop flowerShop7 = tool.findFlowerShop(flowerShops);
                     if (flowerShop7 != null) {
                         flowerShop7.showTotal();
                     }
@@ -105,188 +106,4 @@ public class Main {
             }
         } while (option != 0);
     }
-
-    public static ArrayList<FlowerShop> readFile() {
-        Connection connection = Connection.getInstance();
-        ArrayList<FlowerShop> flowerShops;
-        try {
-            flowerShops = connection.readFile();
-        } catch (IOException e) {
-            System.out.println("No había datos previos");
-            flowerShops = new ArrayList<>();
-        }
-        return flowerShops;
-    }
-
-    public static void createFlowerShop(ArrayList<FlowerShop> flowerShops) {
-        System.out.println("Introduzca el nombre de la floristería");
-        Scanner sc1 = new Scanner(System.in);
-        String name = sc1.nextLine();
-        FlowerShop flowerShop = new FlowerShop(name);
-        boolean found = flowerShops.contains(flowerShop);
-
-        if (found) {
-            System.out.println("La floristería" + name + "ya existe");
-        } else {
-            flowerShops.add(flowerShop);
-            System.out.println("La floristería " + name + " ha sido creada.");
-        }
-    }
-
-    public static void writeFile(ArrayList<FlowerShop> flowershops) {
-        Connection connection = Connection.getInstance();
-        connection.writeFile(flowershops);
-    }
-
-    public static void showFlowerShops(ArrayList<FlowerShop> flowerShops) {
-        for (FlowerShop fs : flowerShops) {
-            System.out.println(fs.getName());
-        }
-    }
-
-    public static FlowerShop findFlowerShop(ArrayList<FlowerShop> flowerShops) {
-        Scanner sc = new Scanner(System.in);
-
-        String name = sc.nextLine();
-        FlowerShop flowerShop = new FlowerShop(name);
-        int index = flowerShops.indexOf(flowerShop);
-        if (index != -1) {
-            return flowerShops.get(index);
-        } else {
-            System.out.println("La floristería " + name + " no existe.");
-            return null;
-        }
-
-    }
-
-    public static void addProduct(FlowerShop flowerShop) {
-        int option;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("¿Qué producto quiere añadir?(1-3)");
-        System.out.println("1. Flor \n 2. Árbol \n 3. Decoración");
-        option = sc.nextInt();
-        switch (option) {
-            case 1:
-                flowerShop.getStock().add(addFlower());
-                System.out.println("Flor registrada");
-                break;
-            case 2:
-                flowerShop.getStock().add(addTree());
-                System.out.println("Árbol registrado");
-                break;
-            case 3:
-                flowerShop.getStock().add(addDeco());
-                System.out.println("Decoración registrada");
-                break;
-        }
-    }
-
-    public static Flower addFlower() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca el nombre de la flor");
-        String name = sc.nextLine().toLowerCase();
-        System.out.println("Introduzca el color de la flor");
-        String colour = sc.nextLine().toLowerCase();
-        System.out.println("Introduzca el precio de la flor");
-        double price = sc.nextDouble();
-        return new Flower(name, colour, price);
-    }
-
-    public static Tree addTree() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca el nombre del árbol");
-        String name = sc.nextLine().toLowerCase();
-        System.out.println("Introduzca la altura del árbol");
-        double height = sc.nextDouble();
-        System.out.println("Introduzca el precio del árbol");
-        double price = sc.nextDouble();
-        return new Tree(name, height, price);
-    }
-
-    public static Decoration addDeco() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca el nombre de la decoración");
-        String name = sc.nextLine().toLowerCase();
-        String option;
-        String type = null;
-        do {
-            System.out.println("Introduzca el tipo de material: (M/P)");
-            System.out.println("P. Plástico \n M. Madera");
-            option = sc.nextLine().toUpperCase();
-            if (option.equals("P")) {
-                type = "Plástico";
-            } else if (option.equals("M")) {
-                type = "Madera";
-            } else {
-                System.out.println("Introduzca M o P");
-            }
-        } while (!option.equals("M") && !option.equals("P"));
-        System.out.println("Introduzca el precio de la decoración");
-        double price = sc.nextDouble();
-        return new Decoration(name, type, price);
-    }
-
-    public static void showStock(FlowerShop flowerShop) {
-        ArrayList<Product> stock = flowerShop.getStock();
-        int index = 0;
-        for (Product product : stock) {
-            System.out.println(index + ". " + product.toString());
-            index++;
-        }
-    }
-
-    public static void deleteProduct(FlowerShop flowerShop) {
-        ArrayList<Product> stock = flowerShop.getStock();
-        if (stock.size() == 0) {
-            System.out.println("No hay productos en esta floristería");
-        } else {
-            System.out.println("Estos son los productos disponibles:");
-            showStock(flowerShop);
-            flowerShop.getStock().remove(findProduct(flowerShop));
-            System.out.println("Producto eliminado");
-        }
-    }
-
-    public static Product findProduct(FlowerShop flowerShop) {
-        Scanner sc = new Scanner(System.in);
-        int index = -1;
-        while (index < 0 || index > flowerShop.getStock().size()) {
-            System.out.println("Escriba el número del producto deseado");
-            index = sc.nextInt();
-            if (index < 0 || index > flowerShop.getStock().size()) {
-                System.out.println("El número no es correcto");
-            }
-        }
-        return flowerShop.getStock().get(index);
-    }
-
-    public static void buyVoid(FlowerShop flowerShop, Ticket ticket) {
-        Scanner sc = new Scanner(System.in);
-        String yesNo = null;
-        do {
-            buyProduct(flowerShop, ticket);
-            System.out.println("¿Quiere seguir comprando? (S/N)");
-            yesNo = sc.nextLine().toUpperCase();
-            while (!yesNo.equals("S") && !yesNo.equals("N")) {
-                System.out.println("Comando incorrecto, introduzca S o N");
-                yesNo = sc.nextLine().toUpperCase();
-            }
-        } while (yesNo.equals("S"));
-    }
-
-    public static void buyProduct(FlowerShop flowerShop, Ticket ticket) {
-        ArrayList<Product> stock = flowerShop.getStock();
-        if (stock.size() == 0) {
-            System.out.println("No hay productos en esta floristería");
-        } else {
-            System.out.println("Estos son los productos disponibles");
-            showStock(flowerShop);
-            Product product = findProduct(flowerShop);
-            ticket.getPurchase().add(product);
-            flowerShop.getStock().remove(product);
-            System.out.println("Producto comprado");
-        }
-    }
-
-
 }
--- src/exercici1n1/functionalities/Tool.java
+++ src/exercici1n1/functionalities/Tool.java
@@ -1,7 +1,7 @@
 package exercici1n1.functionalities;
 
 import exercici1n1.connection.Connection;
-import exercici1n1.entities.FlowerShop;
+import exercici1n1.entities.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -45,6 +45,159 @@ public class Tool {
             System.out.println("La floristería " + name + " ha sido creada.");
         }
     }
+    public void writeFile(ArrayList<FlowerShop> flowershops) {
+        connection.writeFile(flowershops);
+    }
+    public void showFlowerShops(ArrayList<FlowerShop> flowerShops) {
+        for (FlowerShop fs : flowerShops) {
+            System.out.println(fs.getName());
+        }
+    }
+
+    public FlowerShop findFlowerShop(ArrayList<FlowerShop> flowerShops) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        FlowerShop flowerShop = new FlowerShop(name);
+        int index = flowerShops.indexOf(flowerShop);
+        if (index != -1) {
+            return flowerShops.get(index);
+        } else {
+            System.out.println("La floristería " + name + " no existe.");
+            return null;
+        }
+
+    }
+
+    public void addProduct(FlowerShop flowerShop) {
+        int option;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Qué producto quiere añadir?(1-3)");
+        System.out.println("1. Flor \n 2. Árbol \n 3. Decoración");
+        option = sc.nextInt();
+        switch (option) {
+            case 1:
+                flowerShop.getStock().add(addFlower());
+                System.out.println("Flor registrada");
+                break;
+            case 2:
+                flowerShop.getStock().add(addTree());
+                System.out.println("Árbol registrado");
+                break;
+            case 3:
+                flowerShop.getStock().add(addDeco());
+                System.out.println("Decoración registrada");
+                break;
+        }
+    }
+
+    public Flower addFlower() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el nombre de la flor");
+        String name = sc.nextLine().toLowerCase();
+        System.out.println("Introduzca el color de la flor");
+        String colour = sc.nextLine().toLowerCase();
+        System.out.println("Introduzca el precio de la flor");
+        double price = sc.nextDouble();
+        return new Flower(name, colour, price);
+    }
+
+    public Tree addTree() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el nombre del árbol");
+        String name = sc.nextLine().toLowerCase();
+        System.out.println("Introduzca la altura del árbol");
+        double height = sc.nextDouble();
+        System.out.println("Introduzca el precio del árbol");
+        double price = sc.nextDouble();
+        return new Tree(name, height, price);
+    }
+
+    public Decoration addDeco() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el nombre de la decoración");
+        String name = sc.nextLine().toLowerCase();
+        String option;
+        String type = null;
+        do {
+            System.out.println("Introduzca el tipo de material: (M/P)");
+            System.out.println("P. Plástico \n M. Madera");
+            option = sc.nextLine().toUpperCase();
+            if (option.equals("P")) {
+                type = "Plástico";
+            } else if (option.equals("M")) {
+                type = "Madera";
+            } else {
+                System.out.println("Introduzca M o P");
+            }
+        } while (!option.equals("M") && !option.equals("P"));
+        System.out.println("Introduzca el precio de la decoración");
+        double price = sc.nextDouble();
+        return new Decoration(name, type, price);
+    }
+
+    public void showStock(FlowerShop flowerShop) {
+        ArrayList<Product> stock = flowerShop.getStock();
+        int index = 0;
+        for (Product product : stock) {
+            System.out.println(index + ". " + product.toString());
+            index++;
+        }
+    }
+
+    public void deleteProduct(FlowerShop flowerShop) {
+        ArrayList<Product> stock = flowerShop.getStock();
+        if (stock.size() == 0) {
+            System.out.println("No hay productos en esta floristería");
+        } else {
+            System.out.println("Estos son los productos disponibles:");
+            showStock(flowerShop);
+            flowerShop.getStock().remove(findProduct(flowerShop));
+            System.out.println("Producto eliminado");
+        }
+    }
+
+    public Product findProduct(FlowerShop flowerShop) {
+        Scanner sc = new Scanner(System.in);
+        int index = -1;
+        while (index < 0 || index > flowerShop.getStock().size()) {
+            System.out.println("Escriba el número del producto deseado");
+            index = sc.nextInt();
+            if (index < 0 || index > flowerShop.getStock().size()) {
+                System.out.println("El número no es correcto");
+            }
+        }
+        return flowerShop.getStock().get(index);
+    }
+
+    public void buyVoid(FlowerShop flowerShop, Ticket ticket) {
+        Scanner sc = new Scanner(System.in);
+        String yesNo = null;
+        do {
+            buyProduct(flowerShop, ticket);
+            System.out.println("¿Quiere seguir comprando? (S/N)");
+            yesNo = sc.nextLine().toUpperCase();
+            while (!yesNo.equals("S") && !yesNo.equals("N")) {
+                System.out.println("Comando incorrecto, introduzca S o N");
+                yesNo = sc.nextLine().toUpperCase();
+            }
+        } while (yesNo.equals("S"));
+    }
+
+    public void buyProduct(FlowerShop flowerShop, Ticket ticket) {
+        ArrayList<Product> stock = flowerShop.getStock();
+        if (stock.size() == 0) {
+            System.out.println("No hay productos en esta floristería");
+        } else {
+            System.out.println("Estos son los productos disponibles");
+            showStock(flowerShop);
+            Product product = findProduct(flowerShop);
+            ticket.getPurchase().add(product);
+            flowerShop.getStock().remove(product);
+            System.out.println("Producto comprado");
+        }
+    }
+
 
 
 
